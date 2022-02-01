9065669eda60bea343273aa44e1e6f48e62f98a9
mkessy <kokokessy@gmail.com>
2022-01-31 17:39:59 -0800

    working on entry point functions to start stream

--- src/stream/tweetStreamTransforms.ts
+++ src/stream/tweetStreamTransforms.ts
@@ -1,14 +1,29 @@
 import { Stream, pipeline, Transform } from "stream";
 import * as E from "fp-ts/Either";
 import * as D from "io-ts/Decoder";
+import { string } from "fp-ts";
 
 type HeartBeat = "hb";
 type JSONObject = any;
 
+export const objectModeStream = new Transform({
+  objectMode: true,
+  transform(chunk, encoding, callback) {
+    callback(null, chunk);
+  },
+});
+
+export const stringifyStream = new Transform({
+  objectMode: true,
+  transform(chunk, encoding, callback) {
+    callback(null, JSON.stringify(chunk) + "\n");
+  },
+});
+
 export const parseToJson = new Transform({
   objectMode: true,
   transform(chunk, endcoding, callback) {
-    callback(null, JSON.stringify(tryParseChunkToJson(chunk)));
+    callback(null, tryParseChunkToJson(chunk));
   },
 });
 
@@ -29,11 +44,7 @@ export const decodeTransformStream = <A>(decoder: D.Decoder<unknown, A>) => {
 };
 
 // if it can't be parsed to JSON assume it is a heartbeat
-const tryParseChunkToJson = (chunk: any): E.Either<HeartBeat, JSONObject> => {
-  try {
-    const parsedChunk: JSONObject = JSON.parse(chunk);
-    return E.right(parsedChunk);
-  } catch (e) {
-    return E.left("hb");
-  }
-};
+const tryParseChunkToJson = E.tryCatchK(
+  (chunk: string) => JSON.parse(chunk),
+  (e) => "hb" as HeartBeat
+);
--- src/stream/twitterStreamAPI.ts
+++ src/stream/twitterStreamAPI.ts
@@ -2,15 +2,38 @@
 
 import { RetryPolicy, RetryStatus } from "retry-ts";
 import * as TE from "fp-ts/TaskEither";
+import * as E from "fp-ts/Either";
 import * as RTE from "fp-ts/ReaderTaskEither";
-import { AxiosHttpClientEnv, AxiosRequiredConfig } from "../types";
+import {
+  AxiosHttpClientEnv,
+  AxiosRequiredConfig,
+  GetRulesResponse,
+  AddRule,
+} from "../types";
+import {
+  DeleteRulesDecoder,
+  GetRulesResponseDecoder,
+  PostAddRulesResponseDecoder,
+} from "../decoders";
 import { ReadableStream } from "node:stream/web";
 import * as dotenv from "dotenv";
 
 import { NewError } from "../Error/Error";
 import { pipe } from "fp-ts/lib/function";
-import { axiosRequest, getData, validateStatus } from "../utils/axiosUtils";
-dotenv.config();
+import {
+  axiosFetchAndDecode,
+  axiosRequest,
+  getData,
+  validateStatus,
+} from "../utils/axiosUtils";
+import * as path from "path";
+import { DecodeError } from "io-ts/lib/Decoder";
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+interface APIConfig {
+  endpoint: string;
+  axiosConfig: AxiosRequiredConfig;
+}
 
 interface TweetStreamConfig {
   streamEndpoint: string;
@@ -18,11 +41,11 @@ interface TweetStreamConfig {
 }
 
 // TODO: remove magic numbers and centralize the config
-const tweetStreamConfig: TweetStreamConfig = {
-  streamEndpoint: "https://api.twitter.com/2/tweets/search/stream",
+const tweetStreamConfig: APIConfig = {
+  endpoint: "https://api.twitter.com/2/tweets/search/stream",
   axiosConfig: {
     headers: {
-      Authorization: `Barer ${process.env.BEARER_TOKEN}`,
+      Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
     },
     method: "get",
     responseType: "stream",
@@ -30,19 +53,56 @@ const tweetStreamConfig: TweetStreamConfig = {
   },
 };
 
-declare function runTweetStream(): () => void;
-
 export const connectToTweetStream: RTE.ReaderTaskEither<
   AxiosHttpClientEnv,
   NewError,
   NodeJS.ReadStream
 > = pipe(
   tweetStreamConfig,
-  ({ streamEndpoint, axiosConfig }) =>
-    axiosRequest(streamEndpoint, axiosConfig),
+  ({ endpoint, axiosConfig }) => axiosRequest(endpoint, axiosConfig),
   RTE.chainEitherKW(validateStatus([200])),
   RTE.chainTaskEitherK(getData)
 );
 
-type RulesResponse = string;
-type Rules = string;
+const getRulesConfig: APIConfig = {
+  endpoint: "https://api.twitter.com/2/tweets/search/stream/rules",
+  axiosConfig: {
+    headers: {
+      Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
+    },
+    method: "get",
+    responseType: "json",
+    timeout: 5000, // need timeout higher than 20 secs for heartbeat
+  },
+};
+
+export const getTweetStreamRules: RTE.ReaderTaskEither<
+  AxiosHttpClientEnv,
+  NewError | DecodeError,
+  GetRulesResponse
+> = pipe(getRulesConfig, ({ endpoint, axiosConfig }) =>
+  axiosFetchAndDecode<GetRulesResponse>(
+    endpoint,
+    axiosConfig,
+    GetRulesResponseDecoder
+  )
+);
+
+const postRulesConfig: APIConfig = {
+  endpoint: "https://api.twitter.com/2/tweets/search/stream/rules",
+  axiosConfig: {
+    headers: {
+      Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
+    },
+    method: "post",
+    responseType: "json",
+    timeout: 5000, // need timeout higher than 20 secs for heartbeat
+  },
+};
+
+export const postTweetStreamRules = (rules: AddRule) =>
+  axiosFetchAndDecode(
+    postRulesConfig.endpoint,
+    { ...postRulesConfig.axiosConfig, data: rules },
+    PostAddRulesResponseDecoder
+  );
