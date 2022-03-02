4cfafd1d563993aa7b0da0a1f67ac80569ef8df1
mkessy <kokokessy@gmail.com>
2022-03-02 15:07:26 -0800

    added database methods

--- lambda-sentiment/src/handlers/db.ts
+++ lambda-sentiment/src/handlers/db.ts
@@ -1,23 +1,34 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+
 import {
-  DynamoDBClient,
-  PutItemCommand,
-  PutItemCommandInput,
-  PutItemCommandOutput,
-} from "@aws-sdk/client-dynamodb";
+  DynamoDBDocumentClient,
+  PutCommand,
+  PutCommandInput,
+  PutCommandOutput,
+} from "@aws-sdk/lib-dynamodb";
 
 import { SentimentDataPoint } from "../types";
-import { pipe } from "fp-ts/function";
 
-import * as RTE from "fp-ts/ReaderTaskEither";
-import * as TE from 'fp-ts/TaskEither';
+import * as TE from "fp-ts/TaskEither";
 
-const saveItem = (client: DynamoDBClient) => TE.tryCatchK(
-    (data: SentimentDataPoint) => client.send()
+const client = new DynamoDBClient({ region: "us-west-2" });
+const ddbDocClient = DynamoDBDocumentClient.from(client);
 
-)
+const SentimentDataPointToDynamoParams = (
+  data: SentimentDataPoint
+): PutCommandInput => ({
+  TableName: process.env.DATABASE_TABLE,
+  Item: {
+    ruleId: data.ruleId,
+    timestamp: data.timestamp,
+    tweets: data.tweets,
+    sentiment: data.sentiment,
+  },
+});
 
-export const saveSentimentDataPoint = (data: SentimentDataPoint) => pipe(
-    RTE.ask<DynamoDBClient>(),
-    RTE.chainTaskEitherKW((client) => )
+export const putItem = TE.tryCatchK(
+  (data: SentimentDataPoint) =>
+    ddbDocClient.send(new PutCommand(SentimentDataPointToDynamoParams(data))),
 
+  (reason: unknown) => String(reason)
 );
