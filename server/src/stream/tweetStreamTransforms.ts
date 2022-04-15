aa8916cf574ffb201b88397123562e4dfed9b1e8
mkessy <kokokessy@gmail.com>
2022-04-14 19:19:29 -0700

    remove unused node transform functions

--- server/src/stream/tweetStreamTransforms.ts
+++ server/src/stream/tweetStreamTransforms.ts
@@ -7,46 +7,6 @@ import { pipe } from "fp-ts/lib/function";
 type HeartBeat = "hb";
 type JSONObject = any;
 
-export const objectModeStream = new Transform({
-  objectMode: true,
-  transform(chunk, encoding, callback) {
-    callback(null, chunk);
-  },
-});
-
-export const stringifyStream = new Transform({
-  objectMode: true,
-  transform(chunk, encoding, callback) {
-    callback(null, JSON.stringify(chunk) + "\n");
-  },
-});
-
-export const parseToJson = new Transform({
-  objectMode: true,
-  transform(chunk, endcoding, callback) {
-    callback(null, tryParseChunkToJson(chunk));
-  },
-});
-
-export const logStream = new Transform({
-  objectMode: true,
-  transform(chunk, encoding, callback) {
-    callback(null, ((obj) => console.log(obj))(chunk));
-  },
-});
-
-// TO-DO
-
-export const decodeTransformStream = <A>(decoder: D.Decoder<unknown, A>) => {
-  return new Transform({
-    objectMode: true,
-    transform(chunk, endcoding, callback) {
-      const parsed = chunk === "hb" ? chunk : decoder.decode(chunk);
-      callback(null, parsed);
-    },
-  });
-};
-
 // if it can't be parsed to JSON assume it is a heartbeat
 export const tryParseChunkToJson = (chunk: any) => {
   try {
